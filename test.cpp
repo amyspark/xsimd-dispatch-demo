0d51d47e8a7408d0e8a5c244e1c99c37a11f7614
L. E. Segovia <amy@amyspark.me>
2022-04-28 19:11:17 -0300

    Add duplicated symbol guard

--- test.cpp
+++ test.cpp
@@ -3,11 +3,11 @@
 #include "main.hpp"
 #include "xsimd_extensions/config/xsimd_arch.hpp"
 
-
-
+#if XSIMD_UNIVERSAL_BUILD_PASS
 template<>
 int test<xsimd::current_arch>()
 {
     std::cout << typeid(xsimd::current_arch).name() << std::endl;
     return 0;
 }
+#endif
